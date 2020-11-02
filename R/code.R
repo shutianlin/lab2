4c4f250d28c8c783716b1375f562d45b5ecac6ce
shutianlin <54730703+shutianlin@users.noreply.github.com>
2020-11-02 19:57:32 +0800

    Update code.R

--- R/code.R
+++ R/code.R
@@ -7,18 +7,17 @@
 #' @export
 #'
 #' @examples pay_boxplot('Average Covered Charges')
-pay_boxplot <- function(type)
+pay_boxplot <- function(type) ## write a function to make a boxplot
 {
   drg %>%
     ggplot(aes(x = `DRG Definition`,
-               y = get(type))) +
-    geom_boxplot() +
-    scale_y_log10() +
-    ylab(paste0(type)) +
-    xlab("DRG Code") +
-    ggtitle(paste0("Boxplot of ", type, " by DRG code"))
+               y = get(type))) + ## according to the type of payments
+    geom_boxplot() + ## create a boxplot
+    scale_y_log10() + ## scale y axis
+    ylab(paste0(type)) + ## add y axis label
+    xlab("DRG Code") + ## add x axis label
+    ggtitle(paste0("Boxplot of ", type, " by DRG code")) ## add title
 }
-
 
 #' Calculates statistics over all of the DRG codes for average Medicare payments
 #'
@@ -28,13 +27,16 @@ pay_boxplot <- function(type)
 #' @export
 #'
 #' @examples cal_stats('Mean')
-cal_stats <- function(stat){
+cal_stats <- function(stat) {
   drg %>%
-    group_by(`DRG Definition`) %>%
-    summarize(Mean = mean(`Average Medicare Payments`),
-              Median = median(`Average Medicare Payments`),
-              Sd = sd(`Average Medicare Payments`)) %>%
-    select(c(`DRG Definition`, paste0(stat)))
+    group_by(`DRG Definition`) %>% ## group by DRG codes
+    summarize(
+      ## calculate statistics
+      Mean = mean(`Average Medicare Payments`), ## calculate mean
+      Median = median(`Average Medicare Payments`), ## calculate median
+      Sd = sd(`Average Medicare Payments`) ## calculate standard deviation
+    ) %>%
+    select(c(`DRG Definition`, paste0(stat))) ## select the statistics according to the input
 
 }
 
